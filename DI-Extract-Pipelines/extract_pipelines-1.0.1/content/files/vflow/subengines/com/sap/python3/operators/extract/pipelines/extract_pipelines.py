b77f107f51446d352281eb082ecb42fde8cd1d80
Minhee Sung <109061211+minheesung@users.noreply.github.com>
2022-12-21 01:22:28 +0100

    Update extract_pipelines.py

--- DI-Extract-Pipelines/extract_pipelines-1.0.1/content/files/vflow/subengines/com/sap/python3/operators/extract/pipelines/extract_pipelines.py
+++ DI-Extract-Pipelines/extract_pipelines-1.0.1/content/files/vflow/subengines/com/sap/python3/operators/extract/pipelines/extract_pipelines.py
@@ -12,10 +12,10 @@ def get_users(connection):
     restapi = "/auth/v2/user"
     url = connection['url'] + restapi
     headers = {'X-Requested-With': 'XMLHttpRequest'}
-    resp = requests.get(url, headers=headers, auth=connection['auth'], verify=True)
+    resp = requests.get(url, headers=headers, auth=connection['auth'], verify=True, timeout=5)
 
     if resp.status_code != 200:
-        api.logger.error(f"Status code for user {user}: {r.status_code}  - {r.text}")
+        api.logger.error(f"Status code for user {user}: {resp.status_code}  - {resp.text}")
         return []
 
     users = resp.json()
@@ -30,13 +30,26 @@ def get_graphs(connection, user, stopped_after, stopped_before, status):
     restapi = "/app/pipeline-modeler/service/v1/runtime/graphsquery"
     url = connection['url'] + restapi
     headers = {'x-requested-with': 'fetch', 'x-datahub-user': user}
+
     payload = {
-        "filter": ["and", ["greaterThan", "stopped", stopped_after], ["lessOrEqualThan", "stopped", stopped_before],
-                   ["equal", "status", status]]}
-    resp = requests.post(url, headers=headers, auth=connection['auth'], verify=True, data=json.dumps(payload))
+        "filter": ["and", ["greaterThan", "stopped", stopped_after], ["lessOrEqualThan", "stopped", stopped_before]]}
+
+    if ',' in status:
+        status = [x.strip() for x in status.split(',')]
+        status_list = ["or"]
+        for i in range(len(status)):
+            status_list.append(["equal", "status", status[i]])
+        payload['filter'].append(status_list)
+    else:
+        status_list = ["equal", "status"]
+        status_list.append(status)
+        payload['filter'].append(status_list)
+
+    resp = requests.post(url, headers=headers, auth=connection['auth'], verify=True, data=json.dumps(payload),
+                         timeout=5)
 
     if resp.status_code != 200:
-        api.logger.error(f"Status code for user {user}: {r.status_code}  - {r.text}")
+        api.logger.error(f"Status code for user {user}: {resp.status_code}  - {resp.text}")
         return []
 
     graphs = resp.json()
@@ -50,10 +63,10 @@ def get_graph_runtime_details(connection, handle, user):
     restapi = "/app/pipeline-modeler/service/v1/runtime/graphdescriptions"
     url = connection['url'] + restapi + "/" + handle + "?space=tenant"
     headers = {'X-Requested-With': 'XMLHttpRequest', 'x-datahub-user': user}
-    resp = requests.get(url, headers=headers, auth=connection['auth'], verify=True)
+    resp = requests.get(url, headers=headers, auth=connection['auth'], verify=True, timeout=5)
 
     if resp.status_code != 200:
-        api.logger.error(f"Status code for url {url}: {r.status_code}  - {r.text}")
+        api.logger.error(f"Status code for url {url}: {resp.status_code}  - {resp.text}")
         return []
 
     graph_description = resp.json()
@@ -82,7 +95,7 @@ def gen():
 
     cluster = host.replace('https://', '')
     conn = '{"configurationType": "Connection Management", "connectionID": "S3_Catalog"}'
-    size = 999999
+    size = 9999999
     isDir = False
     modTime = '1982-04-15T00:00:00'
 
